95b6d6dbd74aa23cbe2063095170c08840b85f72
fzsofi1-cmd <fzsofi1@gmail.com>
2026-05-06 13:18:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/battag-z-solutions-hub/",
   server: {
     host: "::",
     port: 8080,
